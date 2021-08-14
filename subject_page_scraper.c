ef627b5144628233e470c7e9f50564873ea89d48
Eric Kim <erickim195@gmail.com>
2021-08-14 13:53:11 -0700

    Remove requisites and equivalencies from updated data

--- subject_page_scraper.c
+++ subject_page_scraper.c
@@ -161,9 +161,9 @@ void update_each_course(TidyBuffer* tidy_buffer, size_t* num_courses) {
                 "title", BCON_UTF8(course_title),
                 "description", BCON_UTF8(course_description),
                 "school", BCON_UTF8("UBC"),
-                "preRequisites", BCON_UTF8(prerequisites),
-                "coRequisites", BCON_UTF8(corequisites),
-                "equivalencies", BCON_UTF8(equivalencies), 
+                // "preRequisites", BCON_UTF8(prerequisites),
+                // "coRequisites", BCON_UTF8(corequisites),
+                // "equivalencies", BCON_UTF8(equivalencies), 
             "}"
         );
         opts = mongoc_find_and_modify_opts_new();
